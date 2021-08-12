a0cd7b6dc1ef23f305217ba61c87867889f63338
dududueedu <eduardooffside@gmail.com>
2021-08-12 00:34:24 -0300

    sprites-UPdate

--- Assets/scripts/playerSpt.cs
+++ Assets/scripts/playerSpt.cs
@@ -1,31 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerSpt : MonoBehaviour{
 
     Rigidbody rigidbody;
-    public float forc = 150f;
+    public float jumpForce;
+    //public float forc = 150f;
 
-    // Start is called before the first frame update
     void Start(){
         rigidbody = this.gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update(){
         if(Input.GetKey(KeyCode.LeftArrow)) this.gameObject.transform.Translate( Vector3.left*0.01f );
         else if(Input.GetKey(KeyCode.RightArrow)) this.gameObject.transform.Translate( Vector3.right*0.01f );
-        else if(Input.GetKey(KeyCode.Space)){
-            rigidbody.AddForce(Vector3.up*forc);
-        }
+        //else if(Input.GetKey(KeyCode.Space)){
+        //    rigidbody.AddForce(Vector3.up*forc);
+        //}
         //else if(Input.GetKey(KeyCode.UpArrow)) this.gameObject.transform.Translate( Vector3.forward*0.2f );
         //else if(Input.GetKey(KeyCode.DownArrow)) this.gameObject.transform.Translate( Vector3.forward*-0.1f );
     }
 
-    //void OnCollisionEnter(Collision coll){
-      //  if(coll.gameObject.tag.Equals("bola")){
-        //    coll.transform.position = new Vector3(Random.Range( -5.0f, 5.0f ), 0.5f, Random.Range( -5.0f, 5.0f ));
-        //}
-    //}
+    void OnCollisionEnter(Collision coll){
+        if(coll.gameObject.tag.Equals("HOUSE")){
+            Debug.Log("Vc ganhou!!!");
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    void Jump(){
+        if(Input.GetButtonDown("Jump")){
+            rigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode.Impulse);
+        }
+    }
 }
