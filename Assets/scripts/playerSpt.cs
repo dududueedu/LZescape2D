c9433b41dd87168c877dc0bea70588f5aa76df3c
dududueedu <eduardooffside@gmail.com>
2021-08-11 23:11:23 -0300

    sprites-cenna

--- Assets/scripts/playerSpt.cs
+++ Assets/scripts/playerSpt.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerSpt : MonoBehaviour{
+
+    Rigidbody rigidbody;
+    public float forc = 150f;
+
     // Start is called before the first frame update
     void Start(){
-        
+        rigidbody = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update(){
         if(Input.GetKey(KeyCode.LeftArrow)) this.gameObject.transform.Translate( Vector3.left*0.01f );
         else if(Input.GetKey(KeyCode.RightArrow)) this.gameObject.transform.Translate( Vector3.right*0.01f );
-        else if(Input.GetKey(KeyCode.UpArrow)) this.gameObject.transform.Translate( Vector3.forward*0.2f );
-        else if(Input.GetKey(KeyCode.DownArrow)) this.gameObject.transform.Translate( Vector3.forward*-0.1f );
+        else if(Input.GetKey(KeyCode.Space)){
+            rigidbody.AddForce(Vector3.up*forc);
+        }
+        //else if(Input.GetKey(KeyCode.UpArrow)) this.gameObject.transform.Translate( Vector3.forward*0.2f );
+        //else if(Input.GetKey(KeyCode.DownArrow)) this.gameObject.transform.Translate( Vector3.forward*-0.1f );
     }
 
     //void OnCollisionEnter(Collision coll){
